4ba9a43229111d1562a283a9b88358e4410a62fc
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-03-31 22:56:38 +0300

    Add files via upload

---
+++ 08_mispelled.py
@@ -0,0 +1,13 @@
+# Напишите скрипт, который прверяет неправильное написание слов word1, word2:
+
+# В результате получаем True, если word2 отличается от word1 не более чем на один символ.
+# Если word2 отличается от word1 на два и более, то ответ False.
+# Если в начале или в конце строки добавлен символ, то ответ True в том случае, если добвлено не больше однго символа!
+
+# Например:
+#   word1, word2 = 'versed', 'xersed' # returns True
+#   word1, word2 = 'versed', 'v5rsed' # returns True
+#   word1, word2 = 'versed', 'applb' # returns False
+#   word1, word2 = '1versed', 'versed' # returns True
+#   word1, word2 = 'versed', 'versed' # returns True
+
